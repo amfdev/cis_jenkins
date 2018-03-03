b15f3b615ccc1be3687c06b0ac9fc3252a61a83a
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-03 12:15:59 +0300

    Create cis_checkout_scm.groovy

---
+++ vars/cis_checkout_scm.groovy
@@ -0,0 +1,20 @@
+def call(String branchName, String repoName) {
+    if(branchName != "")
+    {
+        echo "checkout from user branch: ${branchName}; repo: ${repoName}"
+        //checkout([$class: 'GitSCM', branches: [[name: "*/${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [
+        checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [  
+            [$class: 'CleanBeforeCheckout'],
+            [$class: 'CleanCheckout'],
+         //   [$class: 'WipeWorkspace'],
+            [$class: 'CheckoutOption', timeout: 30],
+            [$class: 'CloneOption', timeout: 30, noTags: false],
+            [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
+            ], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'radeonprorender', url: "${repoName}"]]])
+    }
+    else
+    {
+        echo 'checkout from scm options'
+        checkout scm
+    }
+}
