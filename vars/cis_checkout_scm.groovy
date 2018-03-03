3df1975978f2491b4f2024ab6f71e068c0c7b92f
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-03 12:33:39 +0300

    Update cis_checkout_scm.groovy

--- vars/cis_checkout_scm.groovy
+++ vars/cis_checkout_scm.groovy
@@ -2,11 +2,9 @@ def call(String branchName, String repoName) {
     if(branchName != "")
     {
         echo "checkout from user branch: ${branchName}; repo: ${repoName}"
-        //checkout([$class: 'GitSCM', branches: [[name: "*/${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [
         checkout([$class: 'GitSCM', branches: [[name: "${branchName}"]], doGenerateSubmoduleConfigurations: false, extensions: [  
             [$class: 'CleanBeforeCheckout'],
             [$class: 'CleanCheckout'],
-         //   [$class: 'WipeWorkspace'],
             [$class: 'CheckoutOption', timeout: 30],
             [$class: 'CloneOption', timeout: 30, noTags: false],
             [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]
