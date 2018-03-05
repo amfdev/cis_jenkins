5d39d2f0805802d2ad5b88486ca0390fcf1f7a6b
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-05 11:29:27 +0300

    Update amf_ffmpeg_pipeline.groovy

--- vars/amf_ffmpeg_pipeline.groovy
+++ vars/amf_ffmpeg_pipeline.groovy
@@ -20,9 +20,28 @@ def executeBuild(String target, Map options)
         cis_checkout_scm('master', "https://github.com/amfdev/${options.projectName_x264}_scripts.git")
         dir('build')
         {
-            bat"""
-                ubuntu run sh -c './build.sh ${target}' >> ${CIS_LOG} 2>&1
-            """
+            if("${target}" == "mingw_msvc_x64")
+            {
+                bat"""
+                    set INCLUDE=C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\INCLUDE;C:\\Program Files (x86)\\Windows Kits\\8.1\\Include\\um\\;C:\\Program Files (x86)\\Windows Kits\\8.1\\Include\\shared\\;C:\\Program Files (x86)\\Windows Kits\\10\\include\\10.0.10240.0\\ucrt
+                    set LIB=C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\LIB\\amd64;C:\\Program Files (x86)\\Windows Kits\\8.1\\lib\\winv6.3\\um\\x64;C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.10150.0\\ucrt\\x64
+                    ubuntu run sh -c './build.sh ${target}' >> ${CIS_LOG} 2>&1
+                """
+            }else
+            if("${target}" == "mingw_msvc_x86")
+            {
+                bat"""
+                    set INCLUDE=C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\INCLUDE;C:\\Program Files (x86)\\Windows Kits\\8.1\\Include\\um\\;C:\\Program Files (x86)\\Windows Kits\\8.1\\Include\\shared\\;C:\\Program Files (x86)\\Windows Kits\\10\\include\\10.0.10240.0\\ucrt
+                    set LIB=C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\LIB;C:\\Program Files (x86)\\Windows Kits\\8.1\\lib\\winv6.3\\um\\x86;C:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.10150.0\\ucrt\\x86
+                    ubuntu run sh -c './build.sh ${target}' >> ${CIS_LOG} 2>&1
+                """
+            }
+            else
+            {
+                bat"""
+                    ubuntu run sh -c './build.sh ${target}' >> ${CIS_LOG} 2>&1
+                """
+            }
         }
     }
     
