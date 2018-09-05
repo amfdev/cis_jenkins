70b6147e897d9c8c66c11bfbdb3b171d22cc32c2
OvchinnikovDmitrii <ovchinnikov.dmitrii@gmail.com>
2018-09-05 23:23:09 +0300

    Unity fetch script.

--- vars/amf_unity_pipeline.groovy
+++ vars/amf_unity_pipeline.groovy
@@ -37,42 +37,12 @@ def buildSamplesHelper(String target)
 def executeBuild(String target, Map options)
 {
     echo "-------------------------------------executeBuild ${target}-------------------------------------"
-	dir("thirdparty")
-	{
-		cis_checkout_scm('master', "https://github.com/amfdev/thirdparty.git")
-	}
 	dir("Unity")
 	{
 		cis_checkout_scm('master', "https://github.com/amfdev/Unity_dev.git")
-		dir("UnityIntegrations/AmfUnityPlugin")
+		dir("Scripts")
 		{
-			cis_checkout_scm('master', "https://github.com/GPUOpen-LibrariesAndSDKs/UnityIntegrations.git")
-			dir("AmfUnityPlugin")
-			{
-				bat '''
-					set unitytemp=unity-tmp
-
-					where HG
-					IF %ERRORLEVEL% EQU 0 (
-						echo HG found
-						if exist "%unitytemp%" (
-							echo Directory %unitytemp% exists. Please rename or remove.
-						) else (
-							mkdir %unitytemp%
-							pushd %unitytemp%
-							echo Cloning https://bitbucket.org/Unity-Technologies/graphicsdemos
-							hg clone https://bitbucket.org/Unity-Technologies/graphicsdemos
-							popd
-							echo Copying
-							copy \"%unitytemp%/graphicsdemos/NativeRenderingPlugin/PluginSource/source/Unity/*.h\" source/Unity 
-							echo Removing %unitytemp%
-							rd /s /q "%unitytemp%"
-						)
-					) else (
-						echo Aborting, HG binary not found.
-					)
-				'''
-			}
+			Runtime.runtime.exec("fetch_all.bat")
 		}
 		buildPluginHelper(target)
 		buildSamplesHelper(target)
