7453bf0833e873cc3b1ec9056171c8b818ab202c
OvchinnikovDmitrii <ovchinnikov.dmitrii@gmail.com>
2018-09-05 20:06:04 +0300

    no message

--- vars/amf_unity_pipeline.groovy
+++ vars/amf_unity_pipeline.groovy
@@ -47,29 +47,32 @@ def executeBuild(String target, Map options)
 		dir("UnityIntegrations/AmfUnityPlugin")
 		{
 			cis_checkout_scm('master', "https://github.com/GPUOpen-LibrariesAndSDKs/UnityIntegrations.git")
-			bat '''
-				set unitytemp=unity-tmp
+			dir("AmfUnityPlugin")
+			{
+				bat '''
+					set unitytemp=unity-tmp
 
-				where HG
-				IF %ERRORLEVEL% EQU 0 (
-					echo HG found
-					if exist "%unitytemp%" (
-						echo Directory %unitytemp% exists. Please rename or remove.
+					where HG
+					IF %ERRORLEVEL% EQU 0 (
+						echo HG found
+						if exist "%unitytemp%" (
+							echo Directory %unitytemp% exists. Please rename or remove.
+						) else (
+							mkdir %unitytemp%
+							pushd %unitytemp%
+							echo Cloning https://bitbucket.org/Unity-Technologies/graphicsdemos
+							hg clone https://bitbucket.org/Unity-Technologies/graphicsdemos
+							popd
+							echo Copying
+							copy \"%unitytemp%/graphicsdemos/NativeRenderingPlugin/PluginSource/source/Unity/*.h\" source/Unity 
+							echo Removing %unitytemp%
+							rd /s /q "%unitytemp%"
+						)
 					) else (
-						mkdir %unitytemp%
-						pushd %unitytemp%
-						echo Cloning https://bitbucket.org/Unity-Technologies/graphicsdemos
-						hg clone https://bitbucket.org/Unity-Technologies/graphicsdemos
-						popd
-						echo Copying
-						copy \"%unitytemp%/graphicsdemos/NativeRenderingPlugin/PluginSource/source/Unity/*.h\" source/Unity 
-						echo Removing %unitytemp%
-						rd /s /q "%unitytemp%"
+						echo Aborting, HG binary not found.
 					)
-				) else (
-					echo Aborting, HG binary not found.
-				)
-			'''
+				'''
+			}
 		}
 		buildPluginHelper(target)
 		buildSamplesHelper(target)
