2183abae00c6f0f6a4edeffb196d7f86a71a5713
OvchinnikovDmitrii <ovchinnikov.dmitrii@gmail.com>
2018-09-07 02:55:08 +0300

    no message

--- vars/amf_ffmpeg_pipeline.groovy
+++ vars/amf_ffmpeg_pipeline.groovy
@@ -81,7 +81,7 @@ def call(Map userOptions = [:]
         ) {
 
     Map options = [
-        config:'mingw_gcc_x64,mingw_gcc_x86:gpuAMD_RXVEGA',
+        config:'mingw:test',
         
         projectGroup:'AMF',
 
