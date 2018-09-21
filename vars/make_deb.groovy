ff60bd725e8c27be403955641a2fbd59df4177da
OvchinnikovDmitrii <ovchinnikov.dmitrii@gmail.com>
2018-09-21 13:22:14 +0300

    no message

--- vars/make_deb.groovy
+++ vars/make_deb.groovy
@@ -51,7 +51,7 @@ def call(Map userInfo = [:], Map files, List deps) {
 			writeFile file: "control" , text: control
 		}
 	}
-	sh 'fakeroot dpkg-deb --build ' + name + '.deb'
+	sh 'fakeroot dpkg-deb --build ' + name
 	sh 'mv ${name}.deb ' + fullName + '.deb'
 	//validate package
 	sh 'lintian ' + fullName + '.deb'
